3df324c63750c921b5961865dd36b05f59452aaa
Isabella Skořepová <isabella@skorepova.info>
2018-10-12 16:42:48 +0200

    Prettier

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 export type AllPropertiesExcept<T, E> = {
-    [K in keyof T]: K extends E ? never : K
-  }[keyof T]
-  
+  [K in keyof T]: K extends E ? never : K
+}[keyof T]
+
 export type PickExcept<T, E> = Pick<T, AllPropertiesExcept<T, E>>
