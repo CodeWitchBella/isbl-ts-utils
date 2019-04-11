a5d77d2f0bf27aed80f4bb64f53b5a774ee029b6
Isabella Skořepová <isabella@skorepova.info>
2019-04-11 15:49:57 +0200

    Add anotations to make generated typedefs simpler

--- src/index.ts
+++ src/index.ts
@@ -7,3 +7,15 @@ export type PickExcept<T, E> = Pick<T, AllPropertiesExcept<T, E>>
 export const notNull = <T extends Object>(_: T | null): _ is T => _ !== null
 
 export type ArrayElement<T extends any[]> = T extends (infer E)[] ? E : never
+
+export type NullableProperties<T> = {
+  [K in keyof T]: T[K] extends null ? K : never
+}[keyof T]
+export type NonNullableProperties<T> = {
+  [K in keyof T]: T[K] extends null ? never : K
+}[keyof T]
+
+export type NullToOptional<T> = {
+  [k in keyof Pick<T, NullableProperties<T>>]?: T[k]
+} &
+  { [k in keyof Pick<T, NonNullableProperties<T>>]: T[k] }
