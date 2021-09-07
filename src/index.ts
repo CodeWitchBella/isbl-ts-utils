822d9fa2b059eb980d46817b324cff474bafd5dc
Isabella Skořepová <isabella@skorepova.info>
2021-09-07 13:48:13 +0200

    breaking: remove undocumented types

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,6 @@
-export type AllPropertiesExcept<T, E> = {
-  [K in keyof T]: K extends E ? never : K
-}[keyof T]
 
-export type PickExcept<T, E> = Pick<T, AllPropertiesExcept<T, E>>
+export function notNull<T>(_: T | null): _ is T {
+  return _ !== null
+}
 
-export const notNull = <T extends Object>(_: T | null): _ is T => _ !== null
-
-export type ArrayElement<T extends any[]> = T extends (infer E)[] ? E : never
-
-export declare type NullableKeys<T> = {
-  [K in keyof T]: null extends T[K] ? K : never
-}[keyof T]
-
-export declare type NonNullableKeys<T> = {
-  [K in keyof T]: null extends T[K] ? never : K
-}[keyof T]
-
-export declare type NullToOptional<T> = {
-  [k in NullableKeys<T>]?: T[k] | undefined
-} &
-  { [k in NonNullableKeys<T>]: T[k] }
+export type ArrayElement<T extends readonly unknown[]> = T extends readonly (infer E)[] ? E : never
